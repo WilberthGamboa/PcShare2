5b310524a938ababa27c544ba9f0c834df3f8cd5
wilberth <wgcampos15@gmail.com>
2022-12-15 23:29:17 -0600

    agregado a helper

--- controllers/computer-controller.js
+++ controllers/computer-controller.js
@@ -1,8 +1,8 @@
+const { subirArchivo } = require("../helpers/subir-archivo");
 
-const path = require ('path');
-const {v4:uuidv4} = require('uuid');
 
-const uploadComputer=(req,res) =>{
+
+const uploadComputer= async(req,res) =>{
    console.log(req)
 
   if (!req.files || Object.keys(req.files).length === 0) {
@@ -13,36 +13,12 @@ const uploadComputer=(req,res) =>{
     res.status(400).json('No files were uploaded.');
     return;
   }
+  const pathCompleto = await subirArchivo(req.files);
 
+  res.json({
+    path : pathCompleto
+  })
  
-
-  const{archivo} = req.files;
-  const nombreCortado = archivo.name.split('.');
-  const extension = nombreCortado[nombreCortado.length-1];
-  //validar la extension
-  const extensionesValidas = ['png','jpg','jpeg','gif'];  
-
-  if (!extensionesValidas.includes(extension)) {
-    return res.status(400).json({
-      msg: `La extensión ${extension} no es permitida`
-    })
-    
-  }
-
-  const nombreTemp = uuidv4() + '.' + extension;
-
-
-  const uploadPath =path.join(__dirname , '../uploads/' , nombreTemp) ;
-
-  archivo.mv(uploadPath, (err)=> {
-    if (err) {
-      return res.status(500).json({err});
-    }
-
-    res.json({
-        msg:'File uploaded to ' + uploadPath
-    });
-  });
 }
 
 
